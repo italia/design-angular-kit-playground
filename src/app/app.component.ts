c9209a43df72cb782bb7a8e6aade152dbc3c6f94
Andrea Stagi <stagi.andrea@gmail.com>
2024-02-08 19:43:00 +0100

    feat: import angular kit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { ItHeaderComponent, ItIconComponent, ItMegamenuComponent, ItDropdownModule, ItListModule, ItNavBarModule, ItLanguageSwitcherComponent, ItDropdownComponent, ItDropdownItemComponent, ItLinkComponent } from 'design-angular-kit';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, ItHeaderComponent, ItIconComponent, ItMegamenuComponent, ItDropdownComponent, ItDropdownItemComponent, ItListModule, ItNavBarModule, ItLanguageSwitcherComponent, ItLinkComponent]
 })
 export class AppComponent {
   title = 'design-angular-kit-playground';
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideDesignAngularKit } from 'design-angular-kit';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(), // Necessario per il funzionamento della libreria
+    provideHttpClient(), // Necessario per il funzionamento della libreria
+    provideDesignAngularKit(),
+  ]
 };
