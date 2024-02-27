e1fce7e359af199428e4568c1e30168d2f06b5bb
Andrea Stagi <stagi.andrea@gmail.com>
2024-02-27 16:21:04 +0100

    feat: add form with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
-import { ItHeaderComponent, ItIconComponent, ItMegamenuComponent, ItDropdownModule, ItListModule, ItNavBarModule, ItLanguageSwitcherComponent, ItDropdownComponent, ItDropdownItemComponent, ItLinkComponent } from 'design-angular-kit';
+import { ItHeaderComponent, ItIconComponent, ItMegamenuComponent, ItInputComponent, ItListModule, ItNavBarModule, ItLanguageSwitcherComponent, ItDropdownComponent, ItDropdownItemComponent, ItLinkComponent, ItButtonDirective } from 'design-angular-kit';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, UntypedFormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+
+import { ItValidators } from 'design-angular-kit';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [RouterOutlet, ItHeaderComponent, ItIconComponent, ItMegamenuComponent, ItDropdownComponent, ItDropdownItemComponent, ItListModule, ItNavBarModule, ItLanguageSwitcherComponent, ItLinkComponent]
+  imports: [NgIf, RouterOutlet, ItHeaderComponent, ItIconComponent, ItMegamenuComponent, ItDropdownComponent, ItDropdownItemComponent, ItListModule, ItNavBarModule, ItLanguageSwitcherComponent, ItLinkComponent, ItInputComponent, ReactiveFormsModule, ItButtonDirective]
 })
 export class AppComponent {
   title = 'design-angular-kit-playground';
+  myForm: FormGroup;
+
+  constructor(private _fb: FormBuilder) {
+    const validators = [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+      Validators.pattern('[ab]+')
+    ];
+    this.myForm = this._fb.group({
+      taxCode: [null, ItValidators.taxCode],
+      vat: [null, ItValidators.vatNumber],
+      cap: [null, ItValidators.cap],
+      email: [null],
+      url: [null],
+      phone: [null],
+      iban: [null, ItValidators.iban],
+      myInput: ['', validators],
+    });
+  }
+
+  value = '';
+  savedValue = undefined;
+
+  save(form: UntypedFormGroup) {
+    this.savedValue = form.value.myInput;
+  }
+
+  markAllAsTouched() {
+    this.myForm.markAllAsTouched();
+  }
 }
